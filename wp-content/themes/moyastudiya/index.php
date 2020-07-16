0f632801fa8224085a708d9081c5c2090c2386ec
Дмитрий <kuzovlev.dm@gmail.com>
2020-07-16 13:41:46 +0300

    Обновлена структура шаблона, убрана часть лишнего из кода

--- wp-content/themes/moyastudiya/index.php
+++ wp-content/themes/moyastudiya/index.php
@@ -12,46 +12,20 @@
  * @package moyastudiya
  */
 
-get_header();
+//get_header();
 ?>
+    <!doctype html>
+    <html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<main id="primary" class="site-main">
+		<?php wp_head(); ?>
+    </head>
 
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
+    <body>
+    </body>
+    </html>
 <?php
-get_sidebar();
 get_footer();
